673ffc8fd377b29b767d6eec9c76504d68464d6e
poorna0601 <90848857+poorna0601@users.noreply.github.com>
2022-05-10 17:07:19 +0530

    Add files via upload

---
+++ Calculator/calci.js
@@ -0,0 +1,74 @@
+
+function one(val)
+{
+  document.getElementById("calc").value+=val;
+}
+function two(val)
+{
+  document.getElementById("calc").value+=val;
+}
+function three(val)
+{
+  document.getElementById("calc").value+=val;
+}
+function four(val)
+{
+  document.getElementById("calc").value+=val;
+}
+function five(val)
+{
+  document.getElementById("calc").value+=val;
+}
+function six(val)
+{
+  document.getElementById("calc").value+=val;
+}
+function seven(val)
+{
+  document.getElementById("calc").value+=val;
+}
+function eight(val)
+{
+  document.getElementById("calc").value+=val;
+}
+function nine(val)
+{
+  document.getElementById("calc").value+=val;
+}
+function zero(val)
+{
+  document.getElementById("calc").value+=val;
+}
+function sum(val)
+{
+  document.getElementById("calc").value+=val;
+}
+function minus(val)
+{
+  document.getElementById("calc").value+=val;
+}
+function mul(val)
+{
+  document.getElementById("calc").value+=val;
+}
+function div(val)
+{
+  document.getElementById("calc").value+=val;
+}
+function open(val)
+{
+  document.getElementById("calc").value+=val;
+}
+function close(val)
+{
+  document.getElementById("calc").value+=val;
+}
+function clr(val)
+{
+  document.getElementById("calc").value="";
+}
+function result(){
+  var res=document.getElementById("calc").value;
+  var res1=eval(res)
+  document.getElementById("calc").value=res1;
+}
